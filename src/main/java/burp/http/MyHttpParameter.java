fe8df90b624a5d8774910a67cb8b74760500a4db
unknown <11601622+aress31@users.noreply.github.com>
2023-12-26 04:48:19 +0000

    removed unused lombok annotation

--- src/main/java/burp/http/MyHttpParameter.java
+++ src/main/java/burp/http/MyHttpParameter.java
@@ -1,15 +1,11 @@
 package burp.http;
 
 import burp.api.montoya.http.message.params.HttpParameter;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
-@AllArgsConstructor
 @Builder
 @Data
-@EqualsAndHashCode(exclude = "editedValue")
 public class MyHttpParameter {
     private HttpParameter httpParameter;
     private String editedValue;
